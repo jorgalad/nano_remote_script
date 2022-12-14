c2da7c8f6fa90e7844377a6fc0029bb733beb098
JWJ van der Poel <jor@JWJs-MacBook-Air.local>
2022-12-14 07:11:38 +0100

    Device Parameters Working

--- Nano.py
+++ Nano.py
@@ -13,6 +13,7 @@ from _Framework.ModesComponent import AddLayerMode, ModeButtonBehaviour, ModesCo
 from _Framework.SessionComponent import SessionComponent #, SessionRingComponent
 from _Framework.SliderElement import SliderElement
 from _Framework.DeviceComponent import DeviceComponent
+#from .ButtonElement import ButtonElement
 #from .DeviceComponent import DeviceComponent, DeviceModeComponent
 
 #from ableton.v2.control_surface.mode import LayerMode, ModesComponent
@@ -47,10 +48,10 @@ class Nano(ControlSurface):                                     # Create a class
             mixer = self._create_mixer()
             session = self._create_session()
             session.set_mixer(mixer)
-            #self._create_device()
+            device = self._create_device()
             self._create_modes()
+            self.set_device_component(device)
 
-    # _ underscore means it's being called from within the class
     def _create_controls(self):
         def make_button(identifier, name, midi_type=1):
             self.log_message('Creating %s, identifier %s' % (name, identifier))
@@ -74,6 +75,7 @@ class Nano(ControlSurface):                                     # Create a class
             [make_button(48 + i, 'Mute_%d' % (i + 1)) for i in range(8)]])
         self._arm_buttons = ButtonMatrixElement(rows=[
             [make_button(64 + i, 'Mute_%d' % (i + 1)) for i in range(8)]])
+
         self._pan_device_encoders = ButtonMatrixElement(rows=[
             [make_encoder(16 + i, 'Pan_Device_%d' % (i + 1)) for i in range(8)]])
 
@@ -89,23 +91,12 @@ class Nano(ControlSurface):                                     # Create a class
         self._dev_right_btn = make_button(44, 'Dev_Right')
         self._loop_button = make_button(46, 'Cycle')
 
-        #self._dev_mode_button = make_button(60, 'Shift', 0)
-        #self._pan_mode_button = make_button(41, 'Pan', 0)
         # MODE BUTTONS
         self._volume_mode_button = make_button(41, 'Volume_Mode_Button')
-        self._pan_mode_button = make_button(45, 'Pan_Mode_Button')
-
-        #self._pan_device_mode_button = make_button(60, 'Pan_Device_Mode', 0)
-        #self._mute_mode_button = make_button(48, 'Mute_Mode', 0)
-        #self._arm_mode_button = make_button(64, 'Arm_Mode', 0)
+        self._device_mode_button = make_button(45, 'Device_Mode_Button')
 
         self._state_buttons = ButtonMatrixElement(rows=[
           make_button_list(chain(range(73, 77), range(89, 93)), 'Track_State_%d')])
-
-        #self._state_buttons = ButtonMatrixElement(rows=[
-        #    make_button_list(range(73, 77)), 'Track_State_%d')]
-        #self._state_buttons = ButtonMatrixElement(rows=[
-        #    [make_button(64 + i, 'Mute_%d' % (i + 1)) for i in range(8)]])
 
 
     def msg_test(self):
@@ -130,66 +121,36 @@ class Nano(ControlSurface):                                     # Create a class
         self.transport = TransportComponent(self)
         self.transport.set_loop_button(self._loop_button)
 
-
     def _create_device(self):
-        self.log_message('======================= Device Created ======================= ')
-        device = DeviceComponent(name='Device_Component', is_enabled=False, device_selection_follows_track_selection=True)
-        device.layer = Layer(parameter_controls=(self._pan_device_encoders), priority=1)
-        device_settings_layer = Layer(bank_buttons=(self._state_buttons),
-          prev_device_button=(self._dev_left_btn),
-          next_device_button=(self._dev_right_btn),
-          priority=1)
-        mode = DeviceModeComponent(component=device,
-         device_settings_mode=[
-        AddLayerMode(device, device_settings_layer)],
-         is_enabled=True)
-        mode.layer = Layer(device_mode_button=(self._pan_device_mode_button))
-        return device
+        self._device = DeviceComponent(name='Device_Component', is_enabled=True, device_selection_follows_track_selection=True)
+        #device.layer = Layer(parameter_controls=(self._pan_device_encoders), priority=1)
+        #device_settings_layer = Layer(bank_buttons=(self._state_buttons),
+        #  prev_device_button=(self._left_button),
+        #  next_device_button=(self._right_button),
+        #  priority=1)
+        #mode = DeviceModeComponent(component=device,
+        #  device_settings_mode=[
+        # AddLayerMode(device, device_settings_layer)],
+        #  is_enabled=True)
+        #mode.layer = Layer(device_mode_button=(self.device_mode_button))
+        #return device
 
     def _create_mixer(self):
-        # self._session_ring = SessionRingComponent(num_tracks=(self._encoders.width()),
-        #   num_scenes=0,
-        #   is_enabled=False,
-        #   name='Session_Ring')
-        # self._mixer = MixerComponent(tracks_provider=(self._session_ring), name='Mixer')
         self._mixer = MixerComponent(num_tracks, name='Mixer')
-
-    def _create_mixer_2(self):
-        self.mixer = MixerComponent(num_tracks, is_enabled=True, auto_name=True)
-        #mixer.next_track_button = self._track_right_btn
-        #mixer.prev_track_button = self._track_left_btn
-        self.mixer.set_select_buttons(self._track_right_btn, self._track_left_btn)
-        self.mixer.layer = Layer(
-          solo_buttons=self._solo_buttons,
-          mute_buttons=self._mute_buttons,
-          arm_buttons=self._arm_buttons,
-          pan_controls=self._pan_device_encoders,
-          #volume_controls=self._volume_faders
-                            )
-        #mixer.on_send_index_changed = partial(self._show_controlled_sends_message, mixer)
-        for channel_strip in map(self.mixer.channel_strip, range(num_tracks)):
-            channel_strip.empty_color = 'Mixer.NoTrack'
-
-
-
-        # mixer_modes = ModesComponent()
-        # mixer_modes.add_mode('mute', [AddLayerMode(mixer, Layer(mute_buttons=(self._state_buttons)))])
-        # mixer_modes.add_mode('solo', [AddLayerMode(mixer, Layer(solo_buttons=(self._state_buttons)))])
-        # mixer_modes.add_mode('arm', [AddLayerMode(mixer, Layer(arm_buttons=(self._state_buttons)))])
-        # mixer_modes.layer = Layer(
-        #   mute_button=(self._mute_mode_button),
-        #   solo_button=(self._solo_mode_button),
-        #   arm_button=(self._arm_mode_button)),
-        # mixer_modes.selected_mode = 'solo'
-
-
-        return self.mixer
+        self.song().view.selected_track = self._mixer.channel_strip(0)._track       # Make sure we see the first track
+        self._mixer.set_arm_buttons(self._arm_buttons)
+        self._mixer.set_solo_buttons(self._solo_buttons)
+        self._mixer.set_mute_buttons(self._mute_buttons)
 
 
     def _create_modes(self):
         self._modes = ModesComponent(name='Encoder_Modes')
         self._modes.add_mode('volume_mode', LayerMode(self._mixer, Layer(volume_controls=(self._volume_faders))))
-        self._modes.add_mode('pan_mode', LayerMode(self._mixer, Layer(pan_controls=(self._volume_faders))))
+        self._modes.add_mode('device_mode', LayerMode(self._device, Layer(parameter_controls=(self._volume_faders))))
+
+        #self._modes.add_mode('pan_mode', LayerMode(self._mixer, Layer(pan_controls=(self._volume_faders))))
         self._modes.layer = Layer(volume_mode_button=(self._volume_mode_button),
-                                  pan_mode_button=(self._pan_mode_button))
+                                  #pan_mode_button=(self._pan_mode_button),
+                                  device_mode_button=(self._device_mode_button),
+                                  )
         self._modes.selected_mode = 'volume_mode'
